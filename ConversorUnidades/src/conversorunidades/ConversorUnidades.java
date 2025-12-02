07584df681f518395ee9502bc1600d8265327868
dariorumi5 <dariorumi5@alumnos.ilerna.com>
2025-12-02 11:41:17 +0100

    Añadir función de conversión Celsius a Fahrenheit

---
+++ ConversorUnidades/src/conversorunidades/ConversorUnidades.java
@@ -0,0 +1,13 @@
+package conversorunidades;
+
+	public class ConversorUnidades {
+
+	    /**
+	     * Convierte grados Celsius a Fahrenheit
+	     */
+	    public static double celsiusAFahrenheit(double c) {
+	        return c * 9/5 + 32;
+	    }
+	}
+
+
